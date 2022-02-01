85e040cd490a17e845dbb21742dafe8bb20359f8
Valerie Russell <vruss14@gmail.com>
2022-02-01 18:55:06 -0500

    import topojson; convert from topojson to geojson for D3

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ function fetchData() {
     .then(response => {
         educationData = response;
     })
+    
+    // topojson.feature() converts the data from topojson to GeoJSON format for D3
 
     fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json')
     .then(response => response.json())
     .then(response => {
-        countyData = response;
+        countyData = topojson.feature(response, response.objects.counties).features;
         visualizeData(educationData, countyData)
 
     })
@@ -20,6 +22,23 @@ function fetchData() {
 function visualizeData(education, counties) {
     console.log(education);
     console.log(counties);
+
+    let width = 950;
+    let height = 650;
+    let padding = 60;
+
+    const svg = d3.select("#choropleth-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+    svg.selectAll('path')
+    .data(counties)
+    .enter()
+    .append('path')
+    .attr('d', d3.geoPath())
+    .attr('class', 'county')
+
 }
 
 fetchData();
